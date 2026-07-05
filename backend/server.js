747c3fea345a652e17b62cdb2e9dfcabdf4124b4
shubhika30 <shubhikaduhan2327@gmail.com>
2026-07-05 12:00:39 +0530

    Fix render crash

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,11 @@ app.use(cors({
   credentials: true
 }));
 app.use(cors());
-app.options("*", cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(generalLimiter);
 
